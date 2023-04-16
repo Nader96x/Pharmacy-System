840607c023eea811ed938c12dd96ec7e6017aefd
nader mohammed <nader.mohammed.20x@gmail.com>
2023-04-16 17:33:38 +0200

    Medicine Model

--- app/Models/Medicine.php
+++ app/Models/Medicine.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Medicine extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'price',
+        'cost'
+    ];
+
+
 }
