5762427f93bd684d6e26ae959594979987d223d1
nader mohammed <nader.mohammed.20x@gmail.com>
2023-04-21 20:09:47 +0200

    first commit in orders branch

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class AdminSeeder extends Seeder
 {
@@ -19,6 +20,13 @@ class AdminSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+        $this->admin = \App\Models\Admin::first();
+        // send welcome email
+        Mail::raw('This is a simple text', function ($m) {
+            $m->to($this->admin->email)->subject('Email Subject');
+        });
+
+
         // for each admin assign role admin
         $this->command->info('Admin created successfully.');
     }
