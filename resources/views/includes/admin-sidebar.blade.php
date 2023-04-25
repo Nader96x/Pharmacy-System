ee223d4e0f045ee7dcff84d109dd7c3c9bc2abc3
nader mohammed <nader.mohammed.20x@gmail.com>
2023-04-25 19:46:08 +0200

    update sidebar for doctors

--- resources/views/includes/admin-sidebar.blade.php
+++ resources/views/includes/admin-sidebar.blade.php
@@ -77,7 +77,7 @@
                     @endrole
                     @role('admin|owner')
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <a href="{{ route('doctors.index') }}" class="nav-link">
                             <i class="far fa-circle nav-icon"></i>
                             <p>Doctors</p>
                         </a>
